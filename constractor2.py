7ab52804cba4d0e21aaf22c10a4479eabbac9d95
Ripa Das <143581055+ripadas-150@users.noreply.github.com>
2024-08-30 17:44:41 +0600

    Update constractor2.py

--- constractor2.py
+++ constractor2.py
@@ -1,13 +1,30 @@
-class phone :
-    Brand : "Samsung"
-     
-    def __init__(self,num,brand,price ):
-        self.num = num
-        self.brand = brand
-        self.price = price
+class shopping:
+    def __init__(self,name):
+        self.name = name
+        self.cart = []
         
+    def add_to_cart(self,item,price,quantity):
+        product = {"item" : item,"price" : price,"quantity" : quantity} 
+        self.cart.append(product)
     
-    def F(self):
-        print(self.num,self.brand,self.price)
-my_phone = phone(1713861,"samsung",5800)
-my_phone.F()
+    def checkout(self,amount):
+        total = 0
+        for item in self.cart:
+            print(item)
+            total += item["price"] * item["quantity"]
+        print("total price", total )
+        if amount < total :
+            print(f"please provide {total-amount} more")
+        else :
+            extra = amount - total
+            print(f"Here is your extra money {extra}")
+            
+    def remove_item(self,item):
+        pass
+
+    
+ripa = shopping("Ripa")
+ripa.add_to_cart("alu",50,6)
+print(ripa.cart)
+
+ripa.checkout(600)
